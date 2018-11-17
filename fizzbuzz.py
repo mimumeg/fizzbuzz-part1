f419b02e64e5eb35b5a36f93603d69e8afd7958c
mimumeg <meg.stella555@gmail.com>
2018-11-17 14:41:58 +0900

    3の倍数ならFizzを表示するようにした
    PyCharmのReformatCodeがすごかった
    Command + shift + a -> 'reformat code'でいける

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,6 +1,9 @@
 number = int(input("1つ目の自然数を入れてね:"))
 
-# 整数型から文字列型に変換
-output = str(number)
+if number % 3 == 0:
+    output = "Fizz"
+else:
+    # 整数型から文字列型に変換
+    output = str(number)
 
 print(output)
