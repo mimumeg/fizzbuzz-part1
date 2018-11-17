808a2514140cb05bc3185dfb8f2b7ab0bb34daaf
mimumeg <meg.stella555@gmail.com>
2018-11-17 15:07:36 +0900

    15の倍数の条件を「3の倍数かつ5の倍数である」に変更した。
    リファクタリング: 条件判断後にprintで都度表示するようなコードに変更した。

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,13 +1,11 @@
 number = int(input("1つ目の自然数を入れてね:"))
 
-if number % 15 == 0:
-    output = "FizzBuzz"
+if number % 3 == 0 and number % 5 == 0:
+    print("FizzBuzz")
 elif number % 3 == 0:
-    output = "Fizz"
+    print("Fizz")
 elif number % 5 == 0:
-    output = "Buzz"
+    print("Buzz")
 else:
     # 整数型から文字列型に変換
-    output = str(number)
-
-print(output)
+    print(str(number))
